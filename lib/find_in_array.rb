2c551a8c4acd3e60ad48b7e328bbfe643dca5b9a
Susana vik <susana.m.vik@gmail.com>
2021-02-10 00:02:45 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,4 @@
-array = [6, 5, 4, 3, 2, 1]
-
-def find_element_index(array, 2)
+def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
     if array[count] == value_to_find
